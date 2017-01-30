2da695bb82ed9c19789f3f28cc9ef36b1eee2396
Caitlin Hines and Elise St Hilaire <elise@>
2017-01-30 11:37:36 -0800

    add javascript for adding new pet

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Dog(nameOfDog, gender, breed, age, about, picture) {
   this.breed = breed,
   this.age = age,
   this.about = about,
-  this.picture = picture,
+  this.picture = picture
 }
 
 Dog.prototype.displayAllInfo = function() {
@@ -17,18 +17,22 @@ Dog.prototype.displayAllInfo = function() {
 
 //front-end logic
 $(document).ready(function() {
+
   $("form").submit(function(event) {
     event.preventDefault();
+
+    var inputtedName = $("input#name").val();
+    var inputtedGender = $("input:radio[name=gender]:checked").val();
+    var inputtedBreed = $("input#breed").val();
+    var inputtedAge = $("input#age").val();
+    var inputtedAbout = $("input#about").val();
+    var inputtedImageUrl = $("input#image").val();
+
     var newDog = new Dog(inputtedName, inputtedGender, inputtedBreed, inputtedAge, inputtedAbout, inputtedImageUrl);
 
-     var inputtedName = $("input#name").val();
-     var inputtedGender = $("input#gender").val();
-     var inputtedBreed = $("input#breed").val();
-     var inputtedAge = $("input#age").val();
-     var inputtedAbout = $("input#about").val();
-     var inputtedImageUrl = $("input#image").val();
+    alert(inputtedGender);
 
-     $(".added-pets").append("<div class='col-md-4'>" + "<div class='panel-default'>" + "<div class='panel-heading'>" + newDog.nameOfDog + "</div>" + "<div class='panel-body'>" + newDog.displayAllInfo + "</div>" + "</div>" + "</div>");
+     $(".added-pets").append("<div class='col-md-4'>" + "<div class='panel-default'>" + "<div class='panel-heading'>" + newDog.nameOfDog + "</div>" + "<div class='panel-body'>" + "<img src='" + newDog.picture + "'>" + "</div>" + "</div>" + "</div>");
 
 
 
